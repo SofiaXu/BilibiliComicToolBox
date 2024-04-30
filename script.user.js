634375e059bb982ea2ae33abd37b2169ffa77d7f
XueKirby <xuekirby@vip.qq.com>
2024-04-30 23:44:02 +0800

    修改获取漫画价格的逻辑

--- script.user.js
+++ script.user.js
@@ -162,6 +162,34 @@
     tag.className = "b-toolbox-price-tag";
     return tag;
   };
+  const processUnreadManga = async (manga, node) => {
+    const isUnread = manga.last_ep_short_title !== manga.latest_ep_short_title;
+    node.classList.add(isUnread ? 'b-toolbox-manga-card-unread' : 'b-toolbox-manga-card-read');
+  
+    if (isUnread) {
+      try {
+        const priceKey = `manga_price_${manga.comic_id}`;
+        const storedPrice = localStorage.getItem(priceKey);
+  
+        if (storedPrice !== null) {
+          node.appendChild(createPriceTag(parseInt(storedPrice)));
+        } else {
+          const { data: detail } = await api.getEpisodeBuyInfo(manga.latest_ep_id);
+          let price = detail?.pay_gold ?? 0;
+  
+          if (price === 0) {
+            const { data } = await api.getComicDetail(manga.comic_id);
+            price = data?.comic_type === 0 ? 0 : data?.ep_list?.slice(1).find(ep => ep?.pay_gold !== 0)?.pay_gold ?? 0;
+          }
+  
+          localStorage.setItem(priceKey, price.toString());
+          node.appendChild(createPriceTag(price));
+        }
+      } catch (error) {
+        console.error(`获取漫画：${manga.comic_id} 价格失败:`, error);
+      }
+    }
+  };
   const styles = createStyles();
   if (location.pathname.match(/^\/detail\/mc\d+$/)) {
     const createPopupPanel = (styles) => {
@@ -510,15 +538,7 @@
               const id = JSON.parse(node.dataset.biliMangaMsg).manga_id;
               const manga = mangaMap.get(id);
               if (manga) {
-                if (manga.last_ep_short_title !== manga.latest_ep_short_title) {
-                  node.classList.add("b-toolbox-manga-card-unread");
-                  const detail = await api.getComicDetail(id);
-                  const price = detail.data.ep_list[0].pay_gold;
-                  const priceTag = createPriceTag(price);
-                  node.appendChild(priceTag);
-                } else {
-                  node.classList.add("b-toolbox-manga-card-read");
-                }
+                processUnreadManga(manga, node)
               }
             }
           }
@@ -532,11 +552,7 @@
             const id = JSON.parse(node.dataset.biliMangaMsg).manga_id;
             const manga = mangaMap.get(id);
             if (manga) {
-              if (manga.last_ep_short_title !== manga.latest_ep_short_title) {
-                node.classList.add("b-toolbox-manga-card-unread");
-              } else {
-                node.classList.add("b-toolbox-manga-card-read");
-              }
+              processUnreadManga(manga, node)
             }
           }
         }
